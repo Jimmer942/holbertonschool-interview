d5e31a99eb89787242d06ee038541b202eb7d272
Jimmer942 <942@holbertonschool.com>
2021-01-28 15:11:57 -0500

    0-lockboxes.py

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -8,13 +8,13 @@ def canUnlockAll(boxes):
     OUTPUT true if every box can be open flase otherwise
     """
     key_chain = list()
-    room = list()
     key_chain.append(0)
 
     for key in key_chain:
-        for k in boxes[key]:
-            if k not in key_chain:
-                key_chain.append(k)
+        if key < len(boxes):
+            for k in boxes[key]:
+                if k not in key_chain:
+                    key_chain.append(k)
     if len(key_chain) == len(boxes):
         return True
     else:
