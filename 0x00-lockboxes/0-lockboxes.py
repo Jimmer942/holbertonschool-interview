6c6ded59401ee648dcff45bfdb4e3663911ffbee
Jimmer942 <942@holbertonschool.com>
2021-01-29 07:47:29 -0500

    0-lockboxes.py

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -7,15 +7,15 @@ def canUnlockAll(boxes):
     INPUT a list of boxes with it's keys
     OUTPUT true if every box can be open false otherwise
     """
-    if len(boxes) == 0 or len(boxes) == 1:
-        return True
-
     key_chain = [0]
     for key in key_chain:
         if key < len(boxes):
             for open_box in boxes[key]:
-                if open_box not in key_chain:
+                if open_box not in key_chain and open_box < len(boxes):
                     key_chain.append(open_box)
+            if len(key_chain) == len(boxes):
+                return True
+    print(key_chain)
     if len(key_chain) == len(boxes):
         return True
     else:
